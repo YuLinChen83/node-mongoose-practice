122e9ad6a846f18e1cd0889f9bb20d3571067195
Tiffany <tiffany.chen@baifu-tech.net>
2020-05-13 21:18:27 +0800

    step1 connect mongodb atlas db backstage & define restful api

---
+++ server.js
@@ -0,0 +1,26 @@
+import mongoose from 'mongoose';
+import app from './app';
+
+const dotenv = require('dotenv');
+
+dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  })
+  .then(() => console.log('DB connection successful!'))
+  .catch(err => {
+    console.log(`DB connection error: ${ err.message }`);
+  });;
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
+});
